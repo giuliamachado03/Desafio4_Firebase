75bc9e509998c2f1c0e297dc1d73a2fca2daef0c
Giulia Machado <giuliamachado03@gmail.com>
2021-01-27 23:51:42 -0300

    Firebase Login + Register

--- app/src/main/java/com/example/desafiofirebase/LoginFragment.kt
+++ app/src/main/java/com/example/desafiofirebase/LoginFragment.kt
@@ -5,14 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
 
 class LoginFragment : Fragment() {
 
+    private lateinit var _view: View
+    private lateinit var _auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-
-        }
+        _auth = FirebaseAuth.getInstance()
     }
 
     override fun onCreateView(
@@ -23,7 +29,56 @@ class LoginFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
-    companion object {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        _view = view
+
+        _view.findViewById<Button>(R.id.buttonLogin).setOnClickListener {
+            val email = _view.findViewById<EditText>(R.id.editTextLoginEmail)
+            val password = _view.findViewById<EditText>(R.id.editTextLoginPassword)
+
+            realizarLogin(email, password)
+        }
+    }
+
+        private fun realizarLogin(
+
+            email: EditText,
+            password: EditText
+        ) {
+
+            val emailText:String = email.text.toString()
+            val passText = password.text.toString()
+
+                if (!emailText.isBlank()) {
+                    if (!passText.isBlank()) {
+                        realizarLogin(email, password)
+                    } else {
+                        password.error = "O campo senha não pode estar vazio"
+                    }
+                } else {
+                    email.error = "O campo email não pode estar vazio"
+                }
             }
+
+
+         fun fazerLogin(email: String, password: String, name : String) {
+
+            val auth = FirebaseAuth.getInstance()
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val user = FirebaseAuth.getInstance().currentUser
+
+                        val profileUpdates = UserProfileChangeRequest.Builder()
+                            .setDisplayName(name).build()
+
+                        user!!.updateProfile(profileUpdates)
+                    }else{
+                        Toast.makeText(_view.context, it.exception.toString(), Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
     }
--- app/src/main/java/com/example/desafiofirebase/RegisterFragment.kt
+++ app/src/main/java/com/example/desafiofirebase/RegisterFragment.kt
@@ -1,17 +1,13 @@
 package com.example.desafiofirebase
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.google.android.material.button.MaterialButton
-import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.UserProfileChangeRequest
 
