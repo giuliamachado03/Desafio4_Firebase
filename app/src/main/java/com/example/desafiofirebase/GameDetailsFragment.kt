7465bd20db185518c0358696bcb50b601663f4fa
Giulia Machado <giuliamachado03@gmail.com>
2021-01-28 23:48:46 -0300

    Layout telas Game Item, List, Details, Edit

---
+++ app/src/main/java/com/example/desafiofirebase/GameDetailsFragment.kt
@@ -0,0 +1,28 @@
+package com.example.desafiofirebase
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class GameDetailsFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_game_details, container, false)
+    }
+
+    companion object {
+
+                }
+            }
